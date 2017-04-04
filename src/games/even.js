0be28e20f20e2d7229588a219d88d95a48d58fdb
Alexey Efimov <al.efimov@flamp.ru>
2017-04-05 01:06:44 +0300

    add isRight()

--- src/games/even.js
+++ src/games/even.js
@@ -6,15 +6,16 @@ const random = (min, max) => Math.round((min - 0.5) + (Math.random() * ((max - m
 
 const COUNT_QUESTIONS = 3;
 
+const getRight = number => (number % 2 === 0 ? 'yes' : 'no');
+
 const handler = () => {
   for (let i = 0; i < COUNT_QUESTIONS; i += 1) {
     const number = random(0, 100);
-    const correct = (number % 2 === 0) ? 'yes' : 'no';
     const answer = readlineSync.question(`Question: ${number} `);
-    if (answer === correct) {
+    if (answer === getRight(number)) {
       console.log('Correct!');
     } else {
-      console.log(`\'${String(answer)}\' is wrong answer ;(. Correct answer was \'${correct}\'.
+      console.log(`'${String(answer)}' is wrong answer ;(. Correct answer was '${getRight(number)}'.
  Let's try again, ${getName()}!`);
       return;
     }

dbbdb0d94b54a835623bcf7990279c0152a71c7e
Alexey Efimov <al.efimov@flamp.ru>
2017-04-05 10:31:00 +0300

    minor changes

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,39 +4,10 @@ import { newGame } from '../brain-games';
 
 const random = (min, max) => Math.round((min - 0.5) + (Math.random() * ((max - min) + 1)));
 
-const getCorrect = (a, b, op) => {
-  console.log(`operator: ${op}`);
-  switch (op) {
-    case '-':
-      return a - b;
-    case '+':
-      return a + b;
-    case '*':
-      return a * b;
-    default:
-      return NaN;
-  }
-};
-
-const getOperator = () => {
-  const n = Math.floor(Math.random() * 3);
-  switch (n) {
-    case 0:
-      return '-';
-    case 1:
-      return '+';
-    case 2:
-      return '*';
-    default:
-      return NaN;
-  }
-};
-
 const handler = () => {
   const a = random(0, 100);
   const b = random(0, 100);
-  const operator = getOperator();
-  const isCorrect = getCorrect(a, b, operator);
+  const isCorrect = getRight(a, b, operator);
   const answer = readlineSync.question(`Question: ${a} ${operator} ${b} `);
   console.log(`Your answer: ${answer}`);
   if (answer === String(isCorrect)) {
@@ -49,7 +20,7 @@ const handler = () => {
 };
 
 const run = () => {
-  const rules = 'What is the result of the expression?\n';
+  const rules = 'Find the greatest common divisor of given numbers.\n';
   newGame(game(rules, handler));
 };
 
